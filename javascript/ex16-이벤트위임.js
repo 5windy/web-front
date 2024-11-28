b6d88c929a511fa47989c0bc6dc97ac5e059e616
5windy <5windy@naver.com>
2024-11-28 18:36:24 +0900

    Modify todo-list

--- javascript/ex16-이벤트위임.js
+++ javascript/ex16-이벤트위임.js
@@ -1,5 +1,23 @@
 class Todo {
+    #isDone;
+    #content;
 
+    constructor(content, isDone) {
+        this.#content = content;
+        this.#isDone = isDone;
+    }
+
+    // getIsDone = () => this.#isDone;
+
+    // get 또는 set 키워드로 만든 getter와 setter는 내부적으로는 메소드로 동작을 하지만 
+    // 사용을 할 때에는 마치 속성값을 초기화하거나 얻는 것처럼 사용을 함
+    get getIsDone() {
+        return this.#isDone;
+    }   
+    
+    set setIsDone(isDone) {
+        this.#isDone = isDone;
+    }
 }
 
 window.onload = () => {
@@ -15,5 +33,29 @@ window.onload = () => {
     const list = [];
 
     const todoList = document.getElementById("todo-list");
+    console.log(todoList);
+    
+    const ul = todoList.querySelector("ul");
+    console.log(ul);
 
+    const save = todoList.querySelector("#save");
+
+    ul.addEventListener("click", e => {
+        const target = e.target;
+
+    });
+
+    save.addEventListener("click", e => {
+        // 1. input element의 아이디 input-contnet 영역 안에 있는 value를 가져와 
+        // 2. value가 빈 문자열이 아니면 
+        // 3. 아이디 chk로부터 checked 속성 값도 변수로 저장 한 후,
+        // 4. Todo 객체를 생성
+        // 5. list 배열에 추가한 후, 
+        // 6. list 배열의 내용을 기반으로 ul 영역안에 li 엘리먼트 추가
+        // ㄴ 그리기 전, ul의 innterHTML 을 모두 삭제
+
+        const todo = new Todo("오늘의 할 일", false);
+        todo.setIsDone = true;
+        console.log(todo.getIsDone);
+    });
 }
