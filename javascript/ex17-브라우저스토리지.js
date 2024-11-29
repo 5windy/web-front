c90a36dd6219b2c194350d19434e223abc9938b2
5windy <5windy@naver.com>
2024-11-29 11:00:12 +0900

    Add files

---
+++ javascript/ex17-브라우저스토리지.js
@@ -0,0 +1,10 @@
+// BOM : Browser Object Model 
+// ㄴ window.
+
+// 브라우저 스토리지 
+// 1) local storage 
+// ㄴ 브라우저를 종료하더라도 사용자 PC에 계속 남아 있음
+// 2) session storage 
+// ㄴ 브라우저를 종료하면 삭제된다
+// 3) cookie
+// ㄴ 만료기간을 설정할 수 있음
